cf8a3f6cdbd9b0c6d491f9f37a8f60916e8adf72
Anusuya-SK <anusuyak98.Ak@gmail.com>
2025-08-25 13:24:57 +0530

    API error handling

---
+++ fetch-api/src/components/HandleAPIError.js
@@ -0,0 +1,5 @@
+// 🔹 1. Common types of API errors
+// When you make an API call using fetch or axios, errors can come from:
+// Network errors – Internet issues, server down.
+// Client errors (4xx) – Invalid request (e.g., 400, 401, 403, 404).
+// Server errors (5xx) – Backend failure (e.g., 500, 503).
